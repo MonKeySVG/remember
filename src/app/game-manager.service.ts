ad16f84b8e75dc677a8d423b2490ad4f9266acb4
MonKeySVG <play95100@gmail.com>
2024-03-26 19:06:01 +0100

    Compte a rebours

--- src/app/game-manager.service.ts
+++ src/app/game-manager.service.ts
@@ -1,31 +1,51 @@
 import { Injectable } from '@angular/core';
 import {BehaviorSubject} from "rxjs";
 
+export enum GameState {
+  Starting,
+  InProgress,
+  Inactive
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class GameManagerService {
 
-  startCountdownValue: number = 3;
+  startDuration: number = 3;
+  rememberDuration: number = 60;
+
   numberOfCards: number = 50;
+
+  private _gameState = new BehaviorSubject<GameState>(GameState.Inactive);
+  gameState$ = this._gameState.asObservable();
 
   private _cards = new BehaviorSubject<number[]>([]);
   cards$ = this._cards.asObservable();
 
+  private _countdown = new BehaviorSubject<number>(this.startDuration);
+  countdown$ = this._countdown.asObservable();
+
   startGame() {
-    this.startCountdown(this.startCountdownValue);
+    this._gameState.next(GameState.Starting);
+    this.startCountdown(this.startDuration);
     this._cards.next(this.generateCardsArray());
   }
 
   startCountdown(countdown: number) {
-    console.log(countdown);
+    this._countdown.next(countdown);
     const intervalId = setInterval(() => {
       countdown--;
-      if (countdown > 0) {
-        console.log(countdown);
+      if (countdown >= 0) {
+        this._countdown.next(countdown);
       } else {
-        console.log('Go!');
+        if (this._gameState.value === GameState.Starting) {
+          this._gameState.next(GameState.InProgress);
+          // Flip all the cards
+          this.startCountdown(this.rememberDuration);
+        }
         clearInterval(intervalId);
+
       }
     }, 1000);
   }
@@ -38,6 +58,8 @@ export class GameManagerService {
     console.log(array);
     return array;
   }
+
+
 
   constructor() { }
 }
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -9,19 +9,29 @@ import {Subscription} from "rxjs";
 })
 export class GameComponent {
   cards: number[] = [];
+  countdown: number = 0;
 
   private readonly cardsSubscription: Subscription;
+  private readonly countdownSubscription: Subscription;
 
   constructor(private gameManager: GameManagerService) {
     this.cardsSubscription = this.gameManager.cards$.subscribe(cards => {
       this.cards = cards;
       console.log(this.cards);
     });
+
+    this.countdownSubscription = this.gameManager.countdown$.subscribe(countdown => {
+      this.countdown = countdown;
+    });
   }
 
   ngOnDestroy() {
     if (this.cardsSubscription) {
       this.cardsSubscription.unsubscribe();
+    }
+
+    if (this.countdownSubscription) {
+      this.countdownSubscription.unsubscribe();
     }
   }
 
