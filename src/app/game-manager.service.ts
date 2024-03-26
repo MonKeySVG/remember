23a085f35c012a2b94107f42fc156b5eb573b8c1
MonKeySVG <play95100@gmail.com>
2024-03-26 18:43:26 +0100

    Génération de cartes aléatoires

--- src/app/game-manager.service.ts
+++ src/app/game-manager.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
+import {BehaviorSubject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameManagerService {
+
   startCountdownValue: number = 3;
-  numberofCards: number = 50;
+  numberOfCards: number = 50;
+
+  private _cards = new BehaviorSubject<number[]>([]);
+  cards$ = this._cards.asObservable();
 
   startGame() {
     this.startCountdown(this.startCountdownValue);
+    this._cards.next(this.generateCardsArray());
   }
 
   startCountdown(countdown: number) {
@@ -22,6 +28,15 @@ export class GameManagerService {
         clearInterval(intervalId);
       }
     }, 1000);
+  }
+
+  generateCardsArray() {
+    let array = [];
+    for (let i = 0; i < this.numberOfCards; i++) {
+      array.push(Math.floor(Math.random() * 4));
+    }
+    console.log(array);
+    return array;
   }
 
   constructor() { }
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {GameManagerService} from "../game-manager.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-game',
@@ -7,8 +8,22 @@ import {GameManagerService} from "../game-manager.service";
   styleUrl: './game.component.css'
 })
 export class GameComponent {
-  cards = Array(this.gameManager.numberofCards).fill(null);
+  cards: number[] = [];
 
-  constructor(private gameManager: GameManagerService) { }
+  private readonly cardsSubscription: Subscription;
+
+  constructor(private gameManager: GameManagerService) {
+    this.cardsSubscription = this.gameManager.cards$.subscribe(cards => {
+      this.cards = cards;
+      console.log(this.cards);
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.cardsSubscription) {
+      this.cardsSubscription.unsubscribe();
+    }
+  }
+
 
 }
