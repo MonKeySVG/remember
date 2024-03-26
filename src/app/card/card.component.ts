6153d851abd81ab2484476dfaefe092f6ce2ec16
MonKeySVG <play95100@gmail.com>
2024-03-26 18:17:22 +0100

    Affichage des cartes

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -7,7 +7,7 @@ import {Component, Input} from '@angular/core';
 })
 export class CardComponent {
   @Input () color: number = 0;
-  flipped: boolean = false;
+  flipped: boolean = true;
 
   flip() {
     this.flipped = !this.flipped;
